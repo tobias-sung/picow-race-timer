d7e905c079d0fc6cfe6fc3d0363c4c5959166a7d
Tobias Sung <tobias.sung923@gmail.com>
2025-09-15 17:21:12 +0800

    Added GPIO push button functionality.

--- inputs.c
+++ inputs.c
@@ -4,6 +4,17 @@
 #include "stdio.h"
 
 TaskHandle_t uartHandle;
+TaskHandle_t buttonHandle;
+
+//GPIO pin numbers for buttons
+#define BUTTON_1 18 
+#define BUTTON_2 19
+#define BUTTON_3 20
+
+//Global variables for resolving debouncing issue
+unsigned long push_time;
+const int delayTime = 100; 
+
 
 
 //Callback function called whenever UART input is detected
@@ -12,12 +23,30 @@ void vUARTCallback(){
     xTaskNotifyFromISR(uartHandle, input, eSetValueWithOverwrite, NULL);
 }
 
+void vGPIOCallback(uint gpio){
+    //Brief delay to resolve debouncing issue
+     if ((to_ms_since_boot(get_absolute_time()) - push_time)>delayTime) {
+         push_time = to_ms_since_boot(get_absolute_time());
+         xTaskNotifyFromISR(buttonHandle, gpio, eSetValueWithOverwrite, NULL);
+    }
+
+}
+
 void UART_setup(){
     uart_init(uart0, 115200);
     irq_set_exclusive_handler(UART0_IRQ, vUARTCallback);
     irq_set_enabled(UART0_IRQ, true);
     uart_set_irqs_enabled(uart0, 1, 0); //This enables Interrupt outputs for uart0, specifically when RX FIFO contains datapic
 
+}
+
+void GPIO_setup(){
+    gpio_init(BUTTON_1);
+    gpio_init(BUTTON_2);
+    gpio_init(BUTTON_3);
+    gpio_set_irq_enabled_with_callback(BUTTON_1, GPIO_IRQ_EDGE_FALL, 1, &vGPIOCallback);
+    gpio_set_irq_enabled(BUTTON_2, GPIO_IRQ_EDGE_FALL, 1);
+    gpio_set_irq_enabled(BUTTON_3, GPIO_IRQ_EDGE_FALL, 1);
 }
 
 void vUARTTask() {
@@ -50,4 +79,15 @@ void vUARTTask() {
         } 
 
     }
+}
+
+void vButtonTask(void *pvParameters){
+    buttonHandle = xTaskGetCurrentTaskHandle();
+    uint32_t gpio;
+
+    for (;;){
+        xTaskNotifyWait(0, 0, &gpio, portMAX_DELAY);
+        printf("Button %d pressed!\n", gpio);
+    }
+
 }
--- main.c
+++ main.c
@@ -6,12 +6,18 @@ extern void UART_setup();
 extern void vUARTCallback();
 extern void vUARTTask();
 
+extern void GPIO_setup();
+extern void vGPIOCallback();
+extern void vButtonTask();
+
 
 void main() {
     stdio_init_all();
     
     UART_setup();
+    GPIO_setup();
 
     xTaskCreate(vUARTTask, "UART Task", 256, NULL, 1, NULL);
+    xTaskCreate(vButtonTask, "Button Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 }
